ebb2d797bc36f8703933693afda28680dee170b6
manan-bedi2908 <53931841+manan-bedi2908@users.noreply.github.com>
2020-07-31 00:27:09 +0530

    added

--- jarvis.py
+++ jarvis.py
@@ -1,8 +1,11 @@
 import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-print(voices[1].id)
+# print(voices[1].id)
 engine.setProperty('voice', voices[1].id)
 
 
@@ -10,6 +13,39 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+def wishMe():
+
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour <= 12:
+        speak("Good Morning!!")
+    elif hour >=12 and hour <=18:
+        speak("Good Afternoon")
+    else:
+        speak("Good Evening!")
+
+    speak("I am Jarvis, Your personal assistant sir. How may i help you?")
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() == source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print("User said: ", query)
+    except Exception as e:
+        # print(e)
+        print("Couldn't recognize! Kindly speak again")
+        return "None"
+    return query
+
 if __name__ == '__main__':
-    speak("India is the best country")
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+
+        # Logicfor executing tasks based on query
 
