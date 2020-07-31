6a2fbb0f12e327c27821a50d15cd5014cb532f75
manan-bedi2908 <53931841+manan-bedi2908@users.noreply.github.com>
2020-08-01 00:23:36 +0530

    added

--- jarvis.py
+++ jarvis.py
@@ -114,7 +114,18 @@ if __name__ == '__main__':
         elif 'create a new directory' in query:
             try:
                 speak("Creating Directory")
-                os.mkdir('E:\', 'manan.txt)
+                directory = "GeeksForGeeks"
+
+                # Parent Directory path
+                parent_dir = "E:\\HTML FILE"
+
+                # Path
+                path = os.path.join(parent_dir, directory)
+
+                # Create the directory
+                # 'GeeksForGeeks' in
+                # '/home / User / Documents'
+                os.mkdir(path)
             except:
                 speak("Error occured!!")
 
